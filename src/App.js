604b4f96d8f2b722cb5034100440f4460c673004
reddActim <reddactim@gmail.com>
2025-11-17 16:57:58 +0530

    created calcultor along with css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React , {useState} from 'react';
+const buttonLabels = [
+  "7", "8", "9",  "+",
+  "4", "5", "6",  "-",
+  "1", "2", "3", "*",
+  "c", "0", "=", "/"
+];
 function App() {
+  const [expression, setExpression] = useState('');
+  const [result, setResult] = useState();
+  const handleButtonClick = (value) =>{
+    if(value === "c"){
+      setExpression("");
+      setResult(null);
+      return};
+    if(value === "="){
+      try{
+        if(expression === '') setResult("Error");
+        
+      setResult(eval(expression).toString())
+      return;
+      } catch
+      {
+        setResult("Error")
+        return;
+      }
+      };
+
+    setExpression((prev) => (prev + value));
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <h1 className='main-heading'>React Calculator</h1>
+        <input value={expression}/>
+        {result && <div>{result}</div>}
+        <div className='buttons'>
+        {buttonLabels.map((label, index)=> (<button key={index} onClick={() => handleButtonClick(label)}>{label}</button>))}
+        </div>
+      </div>
+    </>
   );
 }
 
